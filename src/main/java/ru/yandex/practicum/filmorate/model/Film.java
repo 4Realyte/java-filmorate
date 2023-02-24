63ee271010377a2a129eaeb164805ee6becec025
IvanB <ibaletov@yandex.ru>
2023-02-24 10:52:45 +0300

    test: add MockMvc

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.filmorate.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
@@ -17,8 +16,8 @@ public class Film {
     private int id;
     @NotBlank(message = "Имя не может быть пустым")
     private String name;
-   @NotBlank
-   @Size(message = "максимальная длина описания — 200 символов", max=200)
+    @NotBlank
+    @Size(message = "максимальная длина описания — 200 символов", max = 200)
     private String description;
     @PastOrPresent
     @JsonFormat(pattern = "yyyy-MM-dd")
--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -1,38 +1,58 @@
 package ru.yandex.practicum.filmorate;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 import ru.yandex.practicum.filmorate.controllers.FilmController;
 import ru.yandex.practicum.filmorate.controllers.UserController;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.User;
 
-import javax.validation.*;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
 import java.io.IOException;
 import java.time.LocalDate;
+import java.util.Collections;
 import java.util.Set;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
-import ru.yandex.practicum.filmorate.model.User;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class FilmorateApplicationTests {
-    ValidatorFactory factory;
-    Validator validator;
-    FilmController filmController;
-    UserController userController;
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
+    private ValidatorFactory factory;
+    private Validator validator;
+    private FilmController filmController;
+    private UserController userController;
 
     @BeforeEach
     void init() {
         factory = Validation.buildDefaultValidatorFactory();
         validator = factory.getValidator();
         filmController = new FilmController();
-        userController =  new UserController();
+        userController = new UserController();
     }
+
     @Test
-    public void createFilm_withoutName() throws IOException, InterruptedException {
+    @SneakyThrows
+    public void createFilm_withoutName() {
         Film film = Film.builder()
                 .id(1)
                 .description("descript")
@@ -42,10 +62,20 @@ class FilmorateApplicationTests {
         Set<ConstraintViolation<Film>> violations = validator.validate(film);
         ConstraintViolation<Film> violation = violations.iterator().next();
         assertEquals("Имя не может быть пустым", violation.getMessage());
+
+        String jsonFilm = objectMapper.writeValueAsString(film);
+        mockMvc.perform(post("/films")
+                        .contentType("application/json")
+                        .content(jsonFilm))
+                .andDo(h -> {
+                    System.out.println(h.getResponse().getStatus());
+                })
+                .andExpect(status().isBadRequest());
     }
 
+    @SneakyThrows
     @Test
-    public void createFilm_withDescriptionOver200Length() throws IOException, InterruptedException {
+    public void createFilm_withDescriptionOver200Length() {
         FilmController controller = new FilmController();
         Film film = Film.builder()
                 .id(1)
@@ -65,25 +95,36 @@ class FilmorateApplicationTests {
         Set<ConstraintViolation<Film>> violations = validator.validate(film);
         ConstraintViolation<Film> violation = violations.iterator().next();
         assertEquals("максимальная длина описания — 200 символов", violation.getMessage());
+
+        String jsonFilm = objectMapper.writeValueAsString(film);
+        mockMvc.perform(post("/films")
+                        .contentType("application/json")
+                        .content(jsonFilm))
+                .andDo(h -> {
+                    System.out.println(h.getResponse().getStatus());
+                })
+                .andExpect(status().isBadRequest());
     }
 
     @Test
-    public void createFilm_Before_28_12_1895() throws IOException, InterruptedException {
-        FilmController controller = new FilmController();
+    @SneakyThrows
+    public void createFilm_Before_28_12_1895() {
+        Film film = Film.builder()
+                .id(1)
+                .name("Titanic")
+                .description("descript")
+                .releaseDate(LocalDate.of(1894, 11, 12))
+                .duration(100)
+                .build();
         ValidationException ex = assertThrows(ValidationException.class, () -> {
-            Film film = Film.builder()
-                    .id(1)
-                    .name("Titanic")
-                    .description("descript")
-                    .releaseDate(LocalDate.of(1894, 11, 12))
-                    .duration(100)
-                    .build();
             filmController.create(film);
         });
         assertEquals("дата релиза — не раньше 28.12.1895", ex.getMessage());
     }
+
     @Test
-    public void createFilm_withNegativeOrZeroDuration() throws IOException, InterruptedException {
+    @SneakyThrows
+    public void createFilm_withNegativeOrZeroDuration() {
         FilmController controller = new FilmController();
         Film film = Film.builder()
                 .id(1)
@@ -107,18 +148,60 @@ class FilmorateApplicationTests {
         assertEquals("Продолжительность фильма не может быть отрицательной или равной нулю", violation.getMessage());
         assertEquals("Продолжительность фильма не может быть отрицательной или равной нулю", violation2.getMessage());
     }
+
     @Test
-    public void createUser_withoutEmail() throws IOException, InterruptedException {
+    @SneakyThrows
+    public void HttpCreateFilm_withNegativeOrZeroDuration() {
+        FilmController controller = new FilmController();
+        Film film = Film.builder().id(1).name("titan").description("descript")
+                .releaseDate(LocalDate.of(1987, 11, 12))
+                .duration(-15)
+                .build();
+        Film film2 = Film.builder().id(2).name("Shrek").description("descript")
+                .releaseDate(LocalDate.of(1987, 10, 12))
+                .duration(0)
+                .build();
+        String jsonFilm = objectMapper.writeValueAsString(film);
+        String jsonFilm2 = objectMapper.writeValueAsString(film2);
+        mockMvc.perform(post("/films")
+                        .contentType("application/json")
+                        .content(jsonFilm))
+                .andDo(h -> {
+                    System.out.println(h.getResponse().getStatus());
+                })
+                .andExpect(status().isBadRequest());
+        mockMvc.perform(post("/films")
+                        .contentType("application/json")
+                        .content(jsonFilm2))
+                .andDo(h -> {
+                    System.out.println(h.getResponse().getStatus());
+                })
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @SneakyThrows
+    public void createUser_withoutEmail() {
         User user = User.builder()
                 .id(1)
                 .name("Alex")
                 .login("Ken")
-                .birthday(LocalDate.of(1994,10,23))
+                .birthday(LocalDate.of(1994, 10, 23))
                 .build();
         Set<ConstraintViolation<User>> violations = validator.validate(user);
         ConstraintViolation<User> violation = violations.iterator().next();
         assertEquals("Электронная почта не может быть пустой", violation.getMessage());
+
+        String jsonUser = objectMapper.writeValueAsString(user);
+        mockMvc.perform(post("/users")
+                        .contentType("application/json")
+                        .content(jsonUser))
+                .andDo(h -> {
+                    System.out.println(h.getResponse().getStatus());
+                })
+                .andExpect(status().isBadRequest());
     }
+
     @Test
     public void createUser_withInvalidEmail() throws IOException, InterruptedException {
         User user = User.builder()
@@ -126,12 +209,13 @@ class FilmorateApplicationTests {
                 .email("@las.same")
                 .name("Alex")
                 .login("Ken")
-                .birthday(LocalDate.of(1994,10,23))
+                .birthday(LocalDate.of(1994, 10, 23))
                 .build();
         Set<ConstraintViolation<User>> violations = validator.validate(user);
         ConstraintViolation<User> violation = violations.iterator().next();
         assertEquals("Эмейл должен быть корректным", violation.getMessage());
     }
+
     @Test
     public void createUser_emptyLogin() throws IOException, InterruptedException {
         User user = User.builder()
@@ -139,23 +223,25 @@ class FilmorateApplicationTests {
                 .email("yandex@mail.com")
                 .name("Alex")
                 .login(" ")
-                .birthday(LocalDate.of(1994,10,23))
+                .birthday(LocalDate.of(1994, 10, 23))
                 .build();
         Set<ConstraintViolation<User>> violations = validator.validate(user);
         ConstraintViolation<User> violation = violations.iterator().next();
         assertEquals("Логин не может быть пустым", violation.getMessage());
     }
+
     @Test
     public void createUser_shouldUseLoginWhenNameIsEmpty() throws IOException, InterruptedException {
         User user = User.builder()
                 .id(1)
                 .email("yandex@mail.com")
                 .login("Nick")
-                .birthday(LocalDate.of(1994,10,23))
+                .birthday(LocalDate.of(1994, 10, 23))
                 .build();
         User createdUser = userController.create(user);
         assertEquals(user.getLogin(), createdUser.getName());
     }
+
     @Test
     public void createUser_withFutureBirthDate() throws IOException, InterruptedException {
         User user = User.builder()
@@ -163,11 +249,19 @@ class FilmorateApplicationTests {
                 .email("yandex@mail.com")
                 .name("Marty")
                 .login("McFly")
-                .birthday(LocalDate.of(2045,10,23))
+                .birthday(LocalDate.of(2045, 10, 23))
                 .build();
         Set<ConstraintViolation<User>> violations = validator.validate(user);
         ConstraintViolation<User> violation = violations.iterator().next();
         assertEquals("дата рождения не может быть в будущем", violation.getMessage());
+    }
+
+    @Test
+    @SneakyThrows
+    public void getUsers_shouldReturnEmptyList() {
+        mockMvc.perform(get("/users"))
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(Collections.emptyList())));
     }
 
 }
