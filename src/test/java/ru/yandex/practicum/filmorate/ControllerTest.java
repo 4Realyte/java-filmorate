b5f4741353047fb2730e82fc769a38fac37841ee
IvanB <ibaletov@yandex.ru>
2023-02-25 11:21:41 +0300

    refactor: create new test structure

--- src/test/java/ru/yandex/practicum/filmorate/ControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/ControllerTest.java
@@ -1,4 +1,28 @@
 package ru.yandex.practicum.filmorate;
 
-public abstract class ControllerTest {
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+abstract class ControllerTest {
+    @Autowired
+    protected MockMvc mockMvc;
+    @Autowired
+    protected ObjectMapper objectMapper;
+    protected ValidatorFactory factory;
+    protected Validator validator;
+    @BeforeEach
+    void init() {
+        factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+    }
 }
---
+++ src/test/java/ru/yandex/practicum/filmorate/UserControllerTest.java
@@ -0,0 +1,107 @@
+package ru.yandex.practicum.filmorate;
+
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import ru.yandex.practicum.filmorate.controllers.UserController;
+import ru.yandex.practicum.filmorate.model.User;
+
+import javax.validation.ConstraintViolation;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.Collections;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class UserControllerTest extends ControllerTest {
+    @Autowired
+    UserController userController;
+    @Test
+    @SneakyThrows
+    public void createUser_withoutEmail() {
+        User user = User.builder()
+                .id(1)
+                .name("Alex")
+                .login("Ken")
+                .birthday(LocalDate.of(1994, 10, 23))
+                .build();
+        Set<ConstraintViolation<User>> violations = validator.validate(user);
+        ConstraintViolation<User> violation = violations.iterator().next();
+        assertEquals("Электронная почта не может быть пустой", violation.getMessage());
+
+        String jsonUser = objectMapper.writeValueAsString(user);
+        mockMvc.perform(post("/users")
+                        .contentType("application/json")
+                        .content(jsonUser))
+                .andDo(h -> {
+                    System.out.println(h.getResponse().getStatus());
+                })
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void createUser_withInvalidEmail() throws IOException, InterruptedException {
+        User user = User.builder()
+                .id(1)
+                .email("@las.same")
+                .name("Alex")
+                .login("Ken")
+                .birthday(LocalDate.of(1994, 10, 23))
+                .build();
+        Set<ConstraintViolation<User>> violations = validator.validate(user);
+        ConstraintViolation<User> violation = violations.iterator().next();
+        assertEquals("Эмейл должен быть корректным", violation.getMessage());
+    }
+
+    @Test
+    public void createUser_emptyLogin() throws IOException, InterruptedException {
+        User user = User.builder()
+                .id(1)
+                .email("yandex@mail.com")
+                .name("Alex")
+                .login(" ")
+                .birthday(LocalDate.of(1994, 10, 23))
+                .build();
+        Set<ConstraintViolation<User>> violations = validator.validate(user);
+        ConstraintViolation<User> violation = violations.iterator().next();
+        assertEquals("Логин не может быть пустым", violation.getMessage());
+    }
+
+    @Test
+    public void createUser_shouldUseLoginWhenNameIsEmpty() throws IOException, InterruptedException {
+        User user = User.builder()
+                .id(1)
+                .email("yandex@mail.com")
+                .login("Nick")
+                .birthday(LocalDate.of(1994, 10, 23))
+                .build();
+        User createdUser = userController.create(user);
+        assertEquals(user.getLogin(), createdUser.getName());
+    }
+
+    @Test
+    public void createUser_withFutureBirthDate() throws IOException, InterruptedException {
+        User user = User.builder()
+                .id(1)
+                .email("yandex@mail.com")
+                .name("Marty")
+                .login("McFly")
+                .birthday(LocalDate.of(2045, 10, 23))
+                .build();
+        Set<ConstraintViolation<User>> violations = validator.validate(user);
+        ConstraintViolation<User> violation = violations.iterator().next();
+        assertEquals("дата рождения не может быть в будущем", violation.getMessage());
+    }
+    @Test
+    @SneakyThrows
+    public void getUsers_shouldReturnEmptyList() {
+        mockMvc.perform(get("/users"))
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(Collections.emptyList())));
+    }
+}
