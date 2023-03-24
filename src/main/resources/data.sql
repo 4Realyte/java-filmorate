8acb0db97feb146c7c2a2f99c4b0beb412662bc7
IvanB <ibaletov@yandex.ru>
2023-03-24 19:55:49 +0300

    feature: added sql schema and data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO mpa_rating (name) VALUES ('G'),('PG'),('PG-13'),('R'),('NC-17');
+INSERT INTO genre (name) VALUES ('Comedy'),('Drama'),('Thriller'),('Documentary'),('Horror'),('Action'),('Cartoon');
